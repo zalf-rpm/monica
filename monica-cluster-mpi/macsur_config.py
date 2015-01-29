445dc97c2a33246fcf1b35c74659fdfad02402b3
Michael Berg <michael.berg@zalf.de>
2015-01-29 16:27:22 +0100

    added cluster/mpi python code to repo in order to simplify use on cluster

---
+++ monica-cluster-mpi/macsur_config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: ISO-8859-15-*-
+
+start_year=1982
+end_year=2011
+
+
+#simulation_types = ["water_limited", "potential", "nutrient_water_limited"]
+simulation_types = ["potential"]
+#resolutions = [10, 25, 50, 100] #1, 10, 
+resolutions = [50]
+
+#simulation_types = ["water_limited"]
+#resolutions = [1]
+
+
+crops =["winter_wheat"] # , "maize"]  # "maize", "winter_wheat"
+
+
+
+
