0e83fd5948204e5d852313147d60d41e6af6c949
Michael Berg <michael.berg@zalf.de>
2016-09-28 15:01:47 +0200

    moved code from run-work-collector.py to renamed file monica_io.py -> optionally added function to add climate data separately to already created env

--- src/python/monica_io.py
+++ src/python/monica_io.py
@@ -1,3 +1,5 @@
+# pragma pylint: disable=mixed-indentation
+
 import time
 import json
 import types
@@ -9,6 +11,143 @@ import monica_python
 
 #print "sys.path: ", sys.path
 #print "sys.version: ", sys.version
+
+
+OP_AVG = 0
+OP_MEDIAN = 1
+OP_SUM = 2
+OP_MIN = 3
+OP_MAX = 4
+OP_FIRST = 5
+OP_LAST = 6
+OP_NONE = 7
+OP_UNDEFINED_OP_ = 8
+
+
+ORGAN_ROOT = 0
+ORGAN_LEAF = 1
+ORGAN_SHOOT = 2
+ORGAN_FRUIT = 3
+ORGAN_STRUCT = 4
+ORGAN_SUGAR = 5
+ORGAN_UNDEFINED_ORGAN_ = 6
+
+
+def oidIsOrgan(oid):
+	return oid["organ"] != ORGAN_UNDEFINED_ORGAN_
+
+
+def oidIsRange(oid):
+	return oid["fromLayer"] >= 0 \
+		and oid["toLayer"] >= 0 \
+		and oid["fromLayer"] < oid["toLayer"] 
+
+
+def opToString(op):
+	return { 
+		  OP_AVG: "AVG"
+		, OP_MEDIAN: "MEDIAN"
+		, OP_SUM: "SUM"
+		, OP_MIN: "MIN"
+		, OP_MAX: "MAX"
+		, OP_FIRST: "FIRST"
+		, OP_LAST: "LAST"
+		, OP_NONE: "NONE"
+		, OP_UNDEFINED_OP_: "undef"
+		}.get(op, "undef")
+
+
+def organToString(organ):
+	return {
+		  ORGAN_ROOT: "Root"
+		, ORGAN_LEAF: "Leaf"
+		, ORGAN_SHOOT: "Shoot"
+		, ORGAN_FRUIT: "Fruit"
+		, ORGAN_STRUCT: "Struct"
+		, ORGAN_SUGAR: "Sugar"
+		, ORGAN_UNDEFINED_ORGAN_: "undef"
+		}.get(organ, "undef")
+
+
+def oidToString(oid, includeTimeAgg):
+	oss = ""
+	oss += "["
+	oss += oid["name"]
+	if oidIsOrgan(oid):
+		oss += ", " + organToString(oid["organ"])
+	elif oidIsRange(oid):
+		oss += ", [" + str(oid["fromLayer"] + 1) + ", " + str(oid["toLayer"] + 1) \
+		+ (", " + opToString(oid["layerAggOp"]) if oid["layerAggOp"] != OP_NONE else "") \
+		+ "]"
+	elif oid["fromLayer"] >= 0:
+		oss += ", " + str(oid["fromLayer"] + 1)
+	if includeTimeAgg:
+		oss += ", " + opToString(oid["timeAggOp"])
+	oss += "]"
+
+	return oss
+
+
+def writeOutputHeaderRows(outputIds, includeHeaderRow = True, includeUnitsRow = True, includeTimeAgg = False):
+	row1 = []
+	row2 = []
+	row3 = []
+	row4 = []
+	for oid in outputIds:
+		fromLayer = oid["fromLayer"]
+		toLayer = oid["toLayer"]
+		isOrgan = oidIsOrgan(oid)
+		isRange = oidIsRange(oid) and oid["layerAggOp"] == OP_NONE
+		if isOrgan:
+			toLayer = fromLayer = oid["organ"] # organ is being represented just by the value of fromLayer currently
+		elif isRange:
+			fromLayer += 1
+			toLayer += 1     # display 1-indexed layer numbers to users
+		else:
+			toLayer = fromLayer # for aggregated ranges, which aren't being displayed as range
+
+		for i in range(fromLayer, toLayer+1):
+			str1 = ""
+			if isOrgan:
+				str1 += oid["name"] + "/" + organToString(oid["organ"])
+			elif isRange:
+				str1 += oid["name"] + "_" + str(i)
+			else:
+				str1 += oid["name"]
+			row1.append(str1)
+			row4.append("j:" + oid["jsonInput"].replace("\"", ""))
+			row3.append("m:" + oidToString(oid, includeTimeAgg))
+			row2.append("[" + oid["unit"] + "]")
+
+	out = []
+	if includeHeaderRow:
+		out.append(row1)
+	if includeUnitsRow:
+		out.append(row4)
+	if includeTimeAgg:
+		out.append(row3)
+		out.append(row2)
+	
+	return out
+
+
+def writeOutput(outputIds, values):
+	out = []
+	if len(values) > 0:
+		for k in range(0, len(values[0])):
+			i = 0
+			row = []
+			for oid in outputIds:
+				j = values[i][k]
+				if type(j) == types.ListType:
+					for jv in j:
+						row.append(jv)
+				else:
+					row.append(j)
+				i += 1
+			out.append(row)
+	return out
+
 
 def isAbsolutePath(p):
 	return p.startswith("/") or p.startswith("\\")
@@ -347,7 +486,7 @@ def supportedPatterns():
 			, "%": percent
 			}
 
-	return supportedPatterns.m;
+	return supportedPatterns.m
 
 
 def printPossibleErrors(es, includeWarnings = False):
@@ -360,6 +499,13 @@ def printPossibleErrors(es, includeWarnings = False):
 			print w
 
 	return es["success"]
+
+
+def parseOutputIds(oids):
+	j = json.dumps(oids)
+	rs = monica_python.parseOutputIdsToJsonString(j)
+	p = json.loads(rs, "latin-1")
+	return p
 
 
 def createEnvJsonFromJsonConfig(cropSiteSim):
@@ -414,12 +560,6 @@ def createEnvJsonFromJsonConfig(cropSiteSim):
 		, "siteParameters": sitej["SiteParameters"]
 	}
 
-	def parseOutputIds(oids):
-		j = json.dumps(oids)
-		rs = monica_python.parseOutputIdsToJsonString(j)
-		p = json.loads(rs, "latin-1")
-		return p
-
 	env["params"] = cpp
 	env["cropRotation"] = cropj["cropRotation"]
 	env["dailyOutputIds"] = parseOutputIds(simj["output"].get("daily", []))
@@ -452,6 +592,29 @@ def createEnvJsonFromJsonConfig(cropSiteSim):
 		with open(simj["climate.csv"]) as f:
 			climateCSVString = f.read()
 
-	env["da"] = json.loads(monica_python.readClimateDataFromCSVStringViaHeadersToJsonString(climateCSVString, json.dumps(options)))
+	if climateCSVString:
+		env["da"] = json.loads(monica_python.readClimateDataFromCSVStringViaHeadersToJsonString(climateCSVString, json.dumps(options)))
+
+	return env
+
+def addClimateDataToEnv(env, simj, climateCSVString = ""):
+	#get no of climate file header lines from sim.json, but prefer from params map
+	climateDataSettings = simj["climate.csv-options"]
+
+	options = {}
+	options["separator"] = defaultValue(climateDataSettings, "csv-separator", ",")
+	options["noOfHeaderLines"] = defaultValue(climateDataSettings, "no-of-climate-file-header-lines", 2)
+	options["headerName2ACDName"] = climateDataSettings["header-to-acd-names"]
+	options["startDate"] = simj["start-date"]
+	options["endDate"] = simj["end-date"]
+	options["useLeapYears"] = simj["use-leap-years"]
+	#print "startDate:", options["startDate"], "endDate:", options["endDate"]
+
+	if not climateCSVString:
+		with open(simj["climate.csv"]) as f:
+			climateCSVString = f.read()
+
+	if climateCSVString:
+		env["da"] = json.loads(monica_python.readClimateDataFromCSVStringViaHeadersToJsonString(climateCSVString, json.dumps(options)))
 
 	return env
