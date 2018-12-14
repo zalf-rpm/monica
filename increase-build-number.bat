6c199c252b445bae508c8e8bb5b43f376fde7d2b
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-12-14 09:53:55 +0100

    cleanup old project files and build number

--- increase-build-number.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set MONICA_BUILD_NUMBER=
-for /f "delims=" %%i in (%1\build-number.txt) do set MONICA_BUILD_NUMBER=%%i
-set /a MONICA_BUILD_NUMBER=%MONICA_BUILD_NUMBER%+1
-echo !define BuildNumber %MONICA_BUILD_NUMBER% > %1\build-number.nsh
-echo %MONICA_BUILD_NUMBER% > %1\build-number.txt
