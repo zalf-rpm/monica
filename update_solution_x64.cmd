608558ccb0af65c4aba114d5c340677f13d380af
sanny <38938782+sannyschulz@users.noreply.github.com>
2019-07-31 16:55:45 +0200

    upgrade VS 2019, removing boost-python

--- update_solution_x64.cmd
+++ update_solution_x64.cmd
@@ -1,4 +1,4 @@
 if not exist _cmake_win64 mkdir _cmake_win64
 cd _cmake_win64
-cmake -G "Visual Studio 15 Win64" .. -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
+cmake -G "Visual Studio 16 2019" -A x64 .. -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
 cd ..
