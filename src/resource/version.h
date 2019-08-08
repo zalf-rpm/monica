3e66177a5fe9bfec65c6e93e70f294767d8eb876
zalffpmbuild <rpm@zalf.de>
2019-08-08 17:48:32 +0200

    auto commit version file version 3.0.2.182

--- src/resource/version.h
+++ src/resource/version.h
@@ -7,7 +7,7 @@
 #define VERSION_MAJOR               3
 #define VERSION_MINOR               0
 #define VERSION_REVISION            2
-#define VERSION_BUILD               181
+#define VERSION_BUILD               182
 
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
 #define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
