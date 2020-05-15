323558ae24a9ae870c07b4fa4312f9d5918b7cf1
sanny <38938782+sannyschulz@users.noreply.github.com>
2020-05-15 14:25:59 +0200

    write out env

--- installer/Hohenfinow2/python3/run-producer.py
+++ installer/Hohenfinow2/python3/run-producer.py
@@ -20,68 +20,87 @@ import sys
 import zmq
 import os
 import monica_io3
+import errno
 
 #print(sys.path)
 
 #print("pyzmq version: ", zmq.pyzmq_version(), " zmq version: ", zmq.zmq_version())
-
+PATH_TO_CLIMATE_FILE = "/monica_data/climate-data/climate-min.csv"
 
 def run_producer(server = {"server": None, "port": None}, shared_id = None):
 
-  context = zmq.Context()
-  socket = context.socket(zmq.PUSH)
+    context = zmq.Context()
+    socket = context.socket(zmq.PUSH) # pylint: disable=no-member
 
-  config = {
-      "port": server["port"] if server["port"] else "6666",
-      "server": server["server"] if server["server"] else "localhost",
-      "sim.json": os.path.join(os.path.dirname(__file__), '../sim-min.json'),
-      "crop.json": os.path.join(os.path.dirname(__file__), '../crop-min.json'),
-      "site.json": os.path.join(os.path.dirname(__file__), '../site-min.json'),
-      "climate.csv": os.path.join(os.path.dirname(__file__), '../climate-min.csv'),
-      "shared_id": shared_id 
-  }
-  # read commandline args only if script is invoked directly from commandline
-  if len(sys.argv) > 1 and __name__ == "__main__":
-      for arg in sys.argv[1:]:
-          k, v = arg.split("=")
-          if k in config:
-              config[k] = v
+    config = {
+        "port": server["port"] if server["port"] else "6666",
+        "server": server["server"] if server["server"] else "localhost",
+        "sim.json": os.path.join(os.path.dirname(__file__), '../sim-min.json'),
+        "crop.json": os.path.join(os.path.dirname(__file__), '../crop-min.json'),
+        "site.json": os.path.join(os.path.dirname(__file__), '../site-min.json'),
+        "climate.csv": PATH_TO_CLIMATE_FILE,
+        "debugout": "/out",
+        "writenv": false,
+        "shared_id": shared_id 
+    }
+    # read commandline args only if script is invoked directly from commandline
+    if len(sys.argv) > 1 and __name__ == "__main__":
+        for arg in sys.argv[1:]:
+            k, v = arg.split("=")
+            if k in config:
+                if k == "writenv" :
+                    config[k] = bool(v)
+                else :
+                    config[k] = v
 
-  print("config:", config)
-  
-  socket.connect("tcp://" + config["server"] + ":" + config["port"])
+    print("config:", config)
+    
+    socket.connect("tcp://" + config["server"] + ":" + config["port"])
 
-  with open(config["sim.json"]) as _:
-    sim_json = json.load(_)
+    with open(config["sim.json"]) as _:
+        sim_json = json.load(_)
 
-  with open(config["site.json"]) as _:
-    site_json = json.load(_)
+    with open(config["site.json"]) as _:
+        site_json = json.load(_)
 
-  with open(config["crop.json"]) as _:
-    crop_json = json.load(_)
+    with open(config["crop.json"]) as _:
+        crop_json = json.load(_)
 
-  #with open(config["climate.csv"]) as _:
-  #  climate_csv = _.read()
+    #with open(config["climate.csv"]) as _:
+    #  climate_csv = _.read()
 
-  env = monica_io3.create_env_json_from_json_config({
-      "crop": crop_json,
-      "site": site_json,
-      "sim": sim_json,
-      "climate": "" #climate_csv
-  })
-  env["csvViaHeaderOptions"] = sim_json["climate.csv-options"]
-  env["pathToClimateCSV"] = config["climate.csv"]
+    env = monica_io3.create_env_json_from_json_config({
+        "crop": crop_json,
+        "site": site_json,
+        "sim": sim_json,
+        "climate": "" #climate_csv
+    })
+    env["csvViaHeaderOptions"] = sim_json["climate.csv-options"]
+    env["pathToClimateCSV"] = config["climate.csv"]
 
-  #print(env)
+    #print(env)
 
-  # add shared ID if env to be sent to routable monicas
-  if config["shared_id"]:
-      env["sharedId"] = config["shared_id"]
+    # add shared ID if env to be sent to routable monicas
+    if config["shared_id"]:
+        env["sharedId"] = config["shared_id"]
 
-  socket.send_json(env)
+    if config["writenv"] :
+        filename = "/out/generated_env.json"
+        WriteEnv(filename, env) 
 
-  print("done")
+    socket.send_json(env)
 
+    print("done")
+
+def WriteEnv(filename, env) :
+    if not os.path.exists(os.path.dirname(filename)):
+        try:
+            os.makedirs(os.path.dirname(filename))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+    with open(filename, 'w') as outfile:
+        json.dump(env, outfile)
 
 if __name__ == "__main__":
     run_producer()
