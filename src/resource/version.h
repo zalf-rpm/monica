efc8c2da0641bb4be91420b1a270c3ef9e997123
zalffpmbuild <rpm@zalf.de>
2020-10-13 15:34:42 +0200

    auto commit version file version 3.2.12.202

--- src/resource/version.h
+++ src/resource/version.h
@@ -6,8 +6,8 @@
 
 #define VERSION_MAJOR               3
 #define VERSION_MINOR               2
-#define VERSION_REVISION            11
-#define VERSION_BUILD               201
+#define VERSION_REVISION            12
+#define VERSION_BUILD               202
 
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
 #define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
