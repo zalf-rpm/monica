4f8fa129bd6ada2562c7d55e876e34fe11f7dc28
Michael Berg <michael.berg@zalf.de>
2021-01-05 18:43:06 +0100

    updated example crop-ser.json

--- test-ser.py
+++ test-ser.py
@@ -1,9 +1,9 @@
 import os
 from datetime import date, timedelta
 
-for doy in range(1, 700):
-    sd = date(1991,1,1) + timedelta(days=doy)
-    ed = date(1991,1,1) + timedelta(days=doy+3)
+for doy in range(1, 365):
+    sd = date(1991,9,23) + timedelta(days=doy)
+    ed = date(1991,9,23) + timedelta(days=doy+3)
     os.system("_cmake_win64\Release\monica-run.exe -sd " + sd.isoformat() + " -ed " + ed.isoformat() + " installer\Hohenfinow2\sim-ser.json")
 
 
