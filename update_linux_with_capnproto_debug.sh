37e24f87597840b911bcf3949d7d2234a94616df
Michael Berg-Mohnicke <michael.berg@zalf.de>
2022-06-27 14:03:39 +0200

    cleanup and reduced # of build file init scripts

--- update_linux_with_capnproto_debug.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p _cmake_linux_debug
-cd  _cmake_linux_debug
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_CAPNPROTO=ON
-cd ..
--- update_linux_without_capnproto.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p _cmake_linux
-cd  _cmake_linux
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
-cd ..
