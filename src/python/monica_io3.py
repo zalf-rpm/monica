74b4493b82e0add2790587301e3f9903fda95201
Michael Berg-Mohnicke <michael.berg@zalf.de>
2022-07-22 11:33:43 +0200

    updated Python version of csv output creation

--- src/python/monica_io3.py
+++ src/python/monica_io3.py
@@ -158,22 +158,41 @@ def write_output_header_rows(output_ids,
 
 #------------------------------------------------------------------------------
 
-def write_output(output_ids, values):
+def write_output(output_ids, values, round_ids={}):
     "write actual output lines"
     out = []
     if len(values) > 0:
         for k in range(0, len(values[0])):
             i = 0
             row = []
-            for _ in output_ids:
+            for oid in output_ids:
+                oid_name = oid["displayName"] if len(oid["displayName"]) > 0 else oid["name"]
                 j__ = values[i][k]
                 if isinstance(j__, list):
                     for jv_ in j__:
-                        row.append(jv_)
+                        row.append(round(jv_, round_ids[oid_name]) if oid_name in round_ids else jv_)
                 else:
-                    row.append(j__)
+                    row.append(round(j__, round_ids[oid_name]) if oid_name in round_ids else j__)
                 i += 1
             out.append(row)
+    return out
+
+#------------------------------------------------------------------------------
+
+def write_output_obj(output_ids, values, round_ids={}):
+    "write actual output lines"
+    out = []
+    for obj in values:
+        row = []
+        for oid in output_ids:
+            oid_name = oid["displayName"] if len(oid["displayName"]) > 0 else oid["name"]
+            j__ = obj.get(oid_name, "")
+            if isinstance(j__, list):
+                for jv_ in j__:
+                    row.append(round(jv_, round_ids[oid_name]) if oid_name in round_ids else jv_)
+            else:
+                row.append(round(j__, round_ids[oid_name]) if oid_name in round_ids else j__)
+        out.append(row)
     return out
 
 #------------------------------------------------------------------------------
