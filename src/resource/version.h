4e65257d9a69f71a970a1ba6625f0ec0ba64f870
Michael Berg-Mohnicke <michael.berg@zalf.de>
2025-03-14 11:01:57 +0100

    version

--- src/resource/version.h
+++ src/resource/version.h
@@ -6,7 +6,7 @@
 
 #define VERSION_MAJOR               3
 #define VERSION_MINOR               6
-#define VERSION_REVISION            33
+#define VERSION_REVISION            34
 #define VERSION_BUILD               0
 
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
