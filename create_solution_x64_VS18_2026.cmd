527f0cb8a064bd2b5218cb88d2a43e937e47fc73
pharosso <37701786+pharosso@users.noreply.github.com>
2026-06-10 15:08:39 +0200

    xxx

---
+++ create_solution_x64_VS18_2026.cmd
@@ -0,0 +1,6 @@
+if not exist _cmake_win64 mkdir _cmake_win64
+rem set MONICA_DIR=%cd%
+cd _cmake_win64
+rem set CMAKE_PREFIX_PATH=%MONICA_DIR%\..\vcpkg\installed\x64-windows\share\unofficial-sodium
+cmake -G "Visual Studio 18 2026" .. -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static 
+cd ..
